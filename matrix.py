3616750792ed39345b971aa070e61871beef1c59
vorpus <li3zhang@gmail.com>
2017-07-12 22:24:14 -0400

    vector implementation

--- matrix.py
+++ matrix.py
@@ -1,11 +1,13 @@
 class Matrix(object):
   def __init__(self, matrix):
-    self.matrix = matrix
-    self.rows = len(matrix)
     if (type(matrix[0]) is list):
+      self.matrix = matrix
+      self.rows = len(matrix)
       self.cols = len(matrix[0])
     else:
-      self.cols = 1
+      self.matrix = [matrix]
+      self.rows = len(self.matrix)
+      self.cols = len(self.matrix[0])
 
   def is_vector(self):
     return self.rows == 1 or self.cols == 1
@@ -22,9 +24,6 @@ class Matrix(object):
       print ""
 
   def transpose(self):
-    # if self.is_vector():
-
-
     new_matrix = []
     for i in range(0, self.cols):
       new_matrix.append([])
--- test_matrix.py
+++ test_matrix.py
@@ -3,9 +3,21 @@ from matrix import Matrix
 
 class TestMatrix(unittest.TestCase):
 
+  def test_init(self):
+
+    test_matrix = Matrix([[1,2,3],[4,5,6]])
+    self.assertEqual(test_matrix.rows, 2)
+    self.assertEqual(test_matrix.cols, 3)
+    self.assertEqual(test_matrix.matrix, [[1,2,3],[4,5,6]])
+
+    test_matrix = Matrix([1,2,3,4])
+    self.assertEqual(test_matrix.rows, 1)
+    self.assertEqual(test_matrix.cols, 4)
+    self.assertEqual(test_matrix.matrix, [[1,2,3,4]])
+
   def test_identity(self):
     test_matrix = Matrix.identity(1)
-    single_identity = [1]
+    single_identity = [[1]]
     self.assertEqual(test_matrix.matrix, single_identity)
 
     test_matrix = Matrix.identity(2)
@@ -21,6 +33,16 @@ class TestMatrix(unittest.TestCase):
     ]
     self.assertEqual(test_matrix.matrix, four_identity)
 
+  def test_is_vector(self):
+    test_matrix = Matrix([1,2,3,4])
+    self.assertTrue(test_matrix.is_vector())
+
+    test_matrix = Matrix([[1],[2],[3]])
+    self.assertTrue(test_matrix.is_vector())
+
+    test_matrix = Matrix([[1,2], [3,4]])
+    self.assertFalse(test_matrix.is_vector())
+
   def test_transpose(self):
     original = Matrix([[1,3,5], [2,4,6]])
     transpose = [[1,2],[3,4],[5,6]]
@@ -30,6 +52,9 @@ class TestMatrix(unittest.TestCase):
     transpose = [[1],[2],[3],[4]]
     self.assertEqual(original.transpose().matrix, transpose)
 
+    original = Matrix([[1],[2],[3],[4]])
+    transpose = [[1,2,3,4]]
+    self.assertEqual(original.transpose().matrix, transpose)
 
 def main():
   unittest.main()
